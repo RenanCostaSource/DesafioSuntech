d3d20604afbe71c2152b7276afb723b45003e84a
DESKTOP-705AG99\Zenic <renan.paiva@lavid.ufpb.br>
2020-11-17 16:12:44 -0300

    versões pre buildadas

---
+++ front/build/precache-manifest.ab4a30a1df1ee867a610ff3563372540.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83cb0283a06039860631624e57f037e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "04fbf7268ba76ce8aaee",
+    "url": "/static/css/main.23b03c58.chunk.css"
+  },
+  {
+    "revision": "5f7f648e05742924e3cd",
+    "url": "/static/js/2.6fe207ef.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.6fe207ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04fbf7268ba76ce8aaee",
+    "url": "/static/js/main.03e830b2.chunk.js"
+  },
+  {
+    "revision": "31b6a70b1e96f32bf4ea",
+    "url": "/static/js/runtime-main.73d46898.js"
+  },
+  {
+    "revision": "e334220e067afbd36d66e032c5131687",
+    "url": "/static/media/bg1.e334220e.jpg"
+  },
+  {
+    "revision": "b46c32e5f57c87d7bd46e97daac8778a",
+    "url": "/static/media/logo-header.b46c32e5.svg"
+  },
+  {
+    "revision": "d02f6594e15302007a3b1a38d575b6e8",
+    "url": "/static/media/pin.d02f6594.png"
+  }
+]);
